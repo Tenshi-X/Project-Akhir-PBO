07f8aad996fdca5b581daa90a134d5f4db9f59fc
Tenshi-X <sevafarel17@gmail.com>
2023-05-18 18:22:08 +0700

    Add Database

---
+++ ticket_reservation.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2023 at 01:21 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ticket_reservation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bioskop`
+--
+
+CREATE TABLE `bioskop` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `poster` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiket_order`
+--
+
+CREATE TABLE `tiket_order` (
+  `id_order` int(11) NOT NULL,
+  `id_film` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_bioskop` int(11) NOT NULL,
+  `jumlah_tiket` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `no_hp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bioskop`
+--
+ALTER TABLE `bioskop`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tiket_order`
+--
+ALTER TABLE `tiket_order`
+  ADD PRIMARY KEY (`id_order`),
+  ADD KEY `id_film` (`id_film`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_bioskop` (`id_bioskop`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bioskop`
+--
+ALTER TABLE `bioskop`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tiket_order`
+--
+ALTER TABLE `tiket_order`
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tiket_order`
+--
+ALTER TABLE `tiket_order`
+  ADD CONSTRAINT `tiket_order_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `film` (`id`),
+  ADD CONSTRAINT `tiket_order_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `tiket_order_ibfk_3` FOREIGN KEY (`id_bioskop`) REFERENCES `bioskop` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
